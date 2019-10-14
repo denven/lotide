daaee5efb1b6c7da175d57ff1339726ef7d9e15c
denven <myregs@foxmail.com>
2019-10-14 16:46:31 -0700

    Assert with emojis

---
+++ assertEqual.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(String.fromCodePoint(0x1F44d) + " Assertion Passed: [" + actual + "] === [" + expected + "]");
+  } else {
+    console.log(String.fromCodePoint(0x1F625) + " Assertion Failed: [" + actual + "] !== [" + expected + "]");
+  }
+};
+
+
+//test code
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+assertEqual("Thanksgiving Day", "Thanksgiving Day");
+assertEqual(1, 2);
+
+//different types arguments outputs wrong result.
+assertEqual("1", 1);
