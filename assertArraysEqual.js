48ca448b005c89ac0ae5f8f5354250ec3b40054c
denven <myregs@foxmail.com>
2019-10-15 19:29:57 -0700

    modify output format

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,8 +1,8 @@
 const eqArrays = function(array1, array2) {
   if (array1.length === array2.length) {
-    array1.forEach(function(item, index) {
-      if (item !== array2[index]) return false;
-    });
+    for (var i=0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) return false;
+    }
   } else {
     return false;
   }
@@ -15,10 +15,10 @@ const assertArraysEqual = function(array1, array2, expected) {
   if (actual === expected) {
     //use template string to output, however, template string cannot display array elements as console.log
     //it removes the brackets and quotation marks arround elements, so I don't use ${array1} to concatenate data.
-    console.log(`${String.fromCodePoint(0x1F44d)}  Assertion Passed: `, array1, " === ", array2);
+    console.log(`${String.fromCodePoint(0x1F44d)}  Assertion Passed: `, array1, "and", array2, expected ? "equals" : "unequals");
   } else {
     //console.log(String.fromCodePoint(0x1F625) + " Assertion Failed: [" + actual + "] !== [" + expected + "]");
-    console.log(`${String.fromCodePoint(0x1F44d)}  Assertion Failed: `, array1, " !== ", array2);
+    console.log(`${String.fromCodePoint(0x1F625)}  Assertion Passed: `, array1, "and", array2, expected ? "equals" : "unequal");
   }
 };
 
