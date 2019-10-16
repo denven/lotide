1eb3bbef31af81275506375e75061f8e3037f9c6
denven <myregs@foxmail.com>
2019-10-15 20:18:48 -0700

    remove Array elements from another array's elements

---
+++ without.js
@@ -0,0 +1,39 @@
+//
+const without = function(srcArray, removeArray) {
+  srcArray.forEach(function(srcItem, index) {
+    removeArray.forEach(function(itemToRemove) {
+      if (srcItem === itemToRemove) {
+        srcArray.splice(index, 1);
+      }
+    });
+  });
+
+  return srcArray;
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) return false;
+    }
+  } else {
+    return false;
+  }
+
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2, expected) {
+  let actual = eqArrays(array1, array2);
+  if (actual === expected) {
+    //use template string to output, however, template string cannot display array elements as console.log
+    //it removes the brackets and quotation marks arround elements, so I don't use ${array1} to concatenate data.
+    console.log(`${String.fromCodePoint(0x1F44d)}  Assertion Passed: Array removed `, array1, expected ? "equals" : "unequals", "expected ", array2);
+  } else {
+    //console.log(String.fromCodePoint(0x1F625) + " Assertion Failed: [" + actual + "] !== [" + expected + "]");
+    console.log(`${String.fromCodePoint(0x1F44d)}  Assertion Failed: Array removed `, array1, expected ? "equals" : "unequals", "expected ", array2);
+  }
+};
+
+assertArraysEqual(without(["1", "2", "3"], ["3"]), ["1", "2"], true);
+assertArraysEqual(without(["1", "2", "3"], ["3", "1"]), ["2"], true);
