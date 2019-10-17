aca36a9441966f6ae35f1ce7a8b5116390575487
denven <myregs@foxmail.com>
2019-10-16 23:18:23 -0700

    console.log format

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -92,7 +92,7 @@ const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect; // <= add this line
   // ...
   if (bEqual) {
-    console.log(`${String.fromCodePoint(0x1F44d)}  Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+    console.log(`${String.fromCodePoint(0x1F44d)} Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
   } else {
     console.log(`${String.fromCodePoint(0x1F626)} Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
   }
