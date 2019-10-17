1fb0e10a663559a17278aae21d835e7722d3a64a
denven <myregs@foxmail.com>
2019-10-16 23:08:49 -0700

    refactor codes

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -86,21 +86,6 @@ const cd2 = {
 };
 //console.log(eqObjects(cd, cd2)); // => false
 
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    //use template string to output
-    console.log(`${String.fromCodePoint(0x1F44d)}  Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    //console.log(String.fromCodePoint(0x1F625) + " Assertion Failed: [" + actual + "] !== [" + expected + "]");
-    console.log(`${String.fromCodePoint(0x1F626)} Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
-
-module.exports = {
-  assertEqual
-};
-
 //test code
 const assertObjectsEqual = function(actual, expected) {
   let bEqual = eqObjects(actual, expected);
