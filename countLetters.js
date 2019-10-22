6d1de929d0d6ceedb36e46923cfb258d74ff796e
denven <myregs@foxmail.com>
2019-10-21 19:41:44 -0700

    tested after commit

--- countLetters.js
+++ countLetters.js
@@ -1,4 +1,6 @@
 //count letters which appear from the input string
+//return an objects which display the count of each letter appears in the string
+//do not counting spaces right now
 const countLetters = function(strToCount) {
   const letterCount = {};
   let strCharArray = strToCount.replace(/\s*/g,"").split("");
@@ -8,17 +10,5 @@ const countLetters = function(strToCount) {
   return letterCount;
 };
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    //use template string to output
-    console.log(`${String.fromCodePoint(0x1F44d)}  Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    //console.log(String.fromCodePoint(0x1F625) + " Assertion Failed: [" + actual + "] !== [" + expected + "]");
-    console.log(`${String.fromCodePoint(0x1F626)} Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
+module.exports = countLetters;
 
-//test code
-console.log(countLetters("lighthouse in the house"));
-//objects cannot be equal directly
-//assertEqual(countLetters("lighthouse in the house"), { l: 1, i: 2, g: 1, h: 4, t: 2, o: 2, u: 2, s: 2, e: 3, n: 1 });
